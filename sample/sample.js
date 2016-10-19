459626a634e73d8dc93fb80a3a2a8d9473d03392
Timo Staudinger <staudinger.timo@gmail.com>
2016-10-19 11:46:27 -0400

    Add sample files to demo syntax highlighting

---
+++ sample/sample.js
@@ -0,0 +1,18 @@
+class Polygon {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  get area() {
+    return this.calcArea();
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const square = new Polygon(10, 10);
+
+console.log(square.area);
---
+++ sample/sample.jsx
@@ -0,0 +1,19 @@
+const CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+const CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
